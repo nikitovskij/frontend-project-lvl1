8d20d87c09cbd3c0b2d7cd33ce9a8cacd28dbeff
nbaranovsky <nbaranovsky24@gmail.com>
2021-06-22 12:06:34 +0200

    refactoring

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,7 @@
 import readline from 'readline-sync';
-import run from './index.js';
 
 export default () => {
   console.log('Welcome to the Brain Games!');
   const name = readline.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
-
-  // TODO: selector of games
-  run();
 };
